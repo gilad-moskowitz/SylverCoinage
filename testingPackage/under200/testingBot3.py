8b065ea979e14716d5c1812c76dc3495d266f992
gilad-moskowitz <69442033+gilad-moskowitz@users.noreply.github.com>
2021-03-16 01:54:19 -0700

    Upgrade to allow for move cap

--- testingPackage/under200/testingBot3.py
+++ testingPackage/under200/testingBot3.py
@@ -5,7 +5,7 @@ class testBot:
     def __init__(self):
         pass
     
-    def nextMove(self, movesPlayed, remainingGaps = []):
+    def nextMove(self, movesPlayed, remainingGaps, cap):
         movesPlayed = [int(i) for i in movesPlayed]
         if (len(movesPlayed) == 0):
             return random.choice([5, 7, 11, 13, 17, 19, 23, 29, 31, 37])
@@ -16,7 +16,7 @@ class testBot:
         elif(len(movesPlayed) == 1):
             factors = PrimeFactorization(movesPlayed[0])
             if (len(factors) == 1):
-                possible = [i for i in range(movesPlayed[0] + 1, max(100, movesPlayed[0] + 1)) if((i%movesPlayed[0]) != 0)]
+                possible = [i for i in range(4, cap) if((i%movesPlayed[0]) != 0)]
                 index = random.randint(0, len(possible) - 1)
                 return possible[index]
             elif(max(factors) > 3):
@@ -25,9 +25,13 @@ class testBot:
                 newMove = 1
                 for a in factors:
                     newMove = newMove*a
-                return int(((newMove/min(factors)) + 1)*min(factors))
+                x = int(((newMove/min(factors)) + 1)*min(factors))
+                if(x < cap):
+                    return x
+                else:
+                    return 15
         else:
-            gcd_moves = gcd(movesPlayed)
+            gcd_moves = gcd_list(movesPlayed)
             if(gcd_moves > 1):
                 newSet = [int(i/gcd_moves) for i in movesPlayed]
                 if (1 in newSet):
@@ -38,8 +42,6 @@ class testBot:
                     return ((remainingMoves[1])*gcd_moves)
                 else:
                     return ((gcd_moves*2 + 1))
-            elif(gcd_moves == 0):
-                return random.randint(4, 100)
             else:
                 if (len(remainingGaps) == 0):
                     S = NumericalSemigroup(movesPlayed)
