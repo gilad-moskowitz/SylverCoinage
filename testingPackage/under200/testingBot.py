c28086bb3310e2e2aeaae187e0a5e94021175e02
gilad-moskowitz <69442033+gilad-moskowitz@users.noreply.github.com>
2021-03-16 00:37:33 -0700

    rewrote the testingBot to play under 200

--- testingPackage/under200/testingBot.py
+++ testingPackage/under200/testingBot.py
@@ -16,7 +16,7 @@ class myBot:
         elif(len(movesPlayed) == 1):
             factors = PrimeFactorization(movesPlayed[0])
             if (len(factors) == 1):
-                possible = [i for i in range(movesPlayed[0] + 1, max(100, movesPlayed[0] + 1)) if((i%movesPlayed[0]) != 0)]
+                possible = [i for i in range(4, 200) if((i%movesPlayed[0]) != 0)]
                 index = random.randint(0, len(possible) - 1)
                 return possible[index]
             elif(max(factors) > 3):
@@ -25,7 +25,11 @@ class myBot:
                 newMove = 1
                 for a in factors:
                     newMove = newMove*a
-                return int(((newMove/min(factors)) + 1)*min(factors))
+                x = int(((newMove/min(factors)) + 1)*min(factors))
+                if(x < 200):
+                    return x
+                else:
+                    return 15
         else:
             gcd_moves = gcd_list(movesPlayed)
             if(gcd_moves == 0):
@@ -42,16 +46,35 @@ class myBot:
                 if((len(remainingMoves) <= 1) and gcd_moves > 1):
                     return (gcd_moves*2 + 1)
                 else:
-                    if((len(remainingMoves)%2) == 0):
-                        return (max(remainingMoves)*gcd_moves)
+                    currentMoveAttempt = max(remainingMoves)*gcd_moves
+                    if (currentMoveAttempt < 200):
+                        if((len(remainingMoves)%2) == 0):
+                            return currentMoveAttempt
+                        else:
+                            linearCombos = [i for i in range(1, max(remainingMoves)) if i not in remainingMoves]
+                            j = len(remainingMoves) - 1
+                            while(j > 2):
+                                for l in linearCombos:
+                                    if (((remainingMoves[j] - l) in remainingMoves) and ((remainingMoves[j] - l) > 3)):
+                                        return ((remainingMoves[j] - l)*gcd_moves)
+                                    else:
+                                        continue
+                                j -= 1
+                            return (max(remainingMoves)*gcd_moves)
                     else:
                         linearCombos = [i for i in range(1, max(remainingMoves)) if i not in remainingMoves]
                         j = len(remainingMoves) - 1
                         while(j > 2):
                             for l in linearCombos:
-                                if (((remainingMoves[j] - l) in remainingMoves) and ((remainingMoves[j] - l) > 3)):
+                                if (((remainingMoves[j] - l) in remainingMoves) and ((remainingMoves[j] - l) > 3) and ((remainingMoves[j] - l)*gcd_moves < 200)):
                                     return ((remainingMoves[j] - l)*gcd_moves)
                                 else:
                                     continue
                             j -= 1
-                        return (max(remainingMoves)*gcd_moves)
+                        t = len(remainingMoves) - 1
+                        while(t > 2):
+                            if(remainingMoves[t]*gcd_moves < 200):
+                                return remainingMoves[t]*gcd_moves
+                            else:
+                                t -= 1
+                        return 1
