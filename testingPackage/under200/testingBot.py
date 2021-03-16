709a92beb4d95de463a85dea6a7683bdbe2e84d6
gilad-moskowitz <69442033+gilad-moskowitz@users.noreply.github.com>
2021-03-16 00:24:08 -0700

    Update testingBot.py

--- testingPackage/under200/testingBot.py
+++ testingPackage/under200/testingBot.py
@@ -1,12 +1,14 @@
 import random
-from semigroupPackage import *
-class testBot:
+from numericalSemigroupLite import *
+
+class myBot:
     def __init__(self):
         pass
     
     def nextMove(self, movesPlayed, remainingGaps = []):
+        movesPlayed = [int(i) for i in movesPlayed]
         if (len(movesPlayed) == 0):
-            return random.randint(4, 30)
+            return random.choice([5, 7, 11, 13, 17, 19, 23, 29, 31, 37])
         elif((3 in movesPlayed) and (2 not in movesPlayed)):
             return 2
         elif((2 in movesPlayed) and (3 not in movesPlayed)):
@@ -26,22 +28,30 @@ class testBot:
                 return int(((newMove/min(factors)) + 1)*min(factors))
         else:
             gcd_moves = gcd_list(movesPlayed)
-            if(gcd_moves > 1):
-                newSet = [int(i/gcd_moves) for i in movesPlayed]
-                if (1 in newSet):
-                    return ((gcd_moves + 1))
-                S = ListByGens(newSet)
-                remainingMoves = gaps(S)
-                if((int((max(remainingMoves)*gcd_moves)) == 2) or (int((max(remainingMoves)*gcd_moves)) == 3)):
-                    return ((gcd_moves*2 + 1))
-                else:
-                    return int((max(remainingMoves)*gcd_moves))
-            elif(gcd_moves == 0):
-                return random.randint(4, 100)
+            if(gcd_moves == 0):
+                return random.randint(4, 30)
             else:
-                if (len(remainingGaps) == 0):
-                    S = ListByGens(movesPlayed)
-                    remainMoves = gaps(S)
-                    return int(max(remainMoves))
+                newSet = [int(i/gcd_moves) for i in movesPlayed]
+                if ((1 in newSet) and (gcd_moves > 1)):
+                    return (gcd_moves*2 + 1)
+                if(len(remainingGaps) == 0):
+                    S = NumericalSemigroup(newSet)
+                    remainingMoves = S.gaps
                 else:
-                    return max(remainingGaps)
+                    remainingMoves = [i for i in remainingGaps]
+                if((len(remainingMoves) <= 1) and gcd_moves > 1):
+                    return (gcd_moves*2 + 1)
+                else:
+                    if((len(remainingMoves)%2) == 0):
+                        return (max(remainingMoves)*gcd_moves)
+                    else:
+                        linearCombos = [i for i in range(1, max(remainingMoves)) if i not in remainingMoves]
+                        j = len(remainingMoves) - 1
+                        while(j > 2):
+                            for l in linearCombos:
+                                if (((remainingMoves[j] - l) in remainingMoves) and ((remainingMoves[j] - l) > 3)):
+                                    return ((remainingMoves[j] - l)*gcd_moves)
+                                else:
+                                    continue
+                            j -= 1
+                        return (max(remainingMoves)*gcd_moves)
