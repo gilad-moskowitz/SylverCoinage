556ecb68059dfce844054488e16a69c6cfba7bbe
gilad-moskowitz <69442033+gilad-moskowitz@users.noreply.github.com>
2021-03-16 01:27:36 -0700

    Updated to adjustable cap

--- testingPackage/under200/testingBot.py
+++ testingPackage/under200/testingBot.py
@@ -5,7 +5,7 @@ class testBot:
     def __init__(self):
         pass
     
-    def nextMove(self, movesPlayed, remainingGaps = []):
+    def nextMove(self, movesPlayed, remainingGaps, cap):
         movesPlayed = [int(i) for i in movesPlayed]
         if (len(movesPlayed) == 0):
             return random.choice([5, 7, 11, 13, 17, 19, 23, 29, 31, 37])
@@ -16,7 +16,7 @@ class testBot:
         elif(len(movesPlayed) == 1):
             factors = PrimeFactorization(movesPlayed[0])
             if (len(factors) == 1):
-                possible = [i for i in range(4, 200) if((i%movesPlayed[0]) != 0)]
+                possible = [i for i in range(4, cap) if((i%movesPlayed[0]) != 0)]
                 index = random.randint(0, len(possible) - 1)
                 return possible[index]
             elif(max(factors) > 3):
@@ -26,7 +26,7 @@ class testBot:
                 for a in factors:
                     newMove = newMove*a
                 x = int(((newMove/min(factors)) + 1)*min(factors))
-                if(x < 200):
+                if(x < cap):
                     return x
                 else:
                     return 15
@@ -47,7 +47,7 @@ class testBot:
                     return (gcd_moves*2 + 1)
                 else:
                     currentMoveAttempt = max(remainingMoves)*gcd_moves
-                    if (currentMoveAttempt < 200):
+                    if (currentMoveAttempt < cap):
                         if((len(remainingMoves)%2) == 0):
                             return currentMoveAttempt
                         else:
@@ -66,14 +66,14 @@ class testBot:
                         j = len(remainingMoves) - 1
                         while(j > 2):
                             for l in linearCombos:
-                                if (((remainingMoves[j] - l) in remainingMoves) and ((remainingMoves[j] - l) > 3) and ((remainingMoves[j] - l)*gcd_moves < 200)):
+                                if (((remainingMoves[j] - l) in remainingMoves) and ((remainingMoves[j] - l) > 3) and ((remainingMoves[j] - l)*gcd_moves < cap)):
                                     return ((remainingMoves[j] - l)*gcd_moves)
                                 else:
                                     continue
                             j -= 1
                         t = len(remainingMoves) - 1
                         while(t > 2):
-                            if(remainingMoves[t]*gcd_moves < 200):
+                            if(remainingMoves[t]*gcd_moves < cap):
                                 return remainingMoves[t]*gcd_moves
                             else:
                                 t -= 1
