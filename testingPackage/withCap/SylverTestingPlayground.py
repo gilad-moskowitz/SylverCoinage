6d2dea7c3e5ffcf24cf2b49a249d0ebb79e91048
gilad-moskowitz <69442033+gilad-moskowitz@users.noreply.github.com>
2021-03-28 02:06:16 -0700

    Update SylverTestingPlayground.py

--- testingPackage/withCap/SylverTestingPlayground.py
+++ testingPackage/withCap/SylverTestingPlayground.py
@@ -1,16 +1,11 @@
 import math
 import random
+from numericalSemigroupLite import*
 from myNewBot import myBot
 from testingBot import testBot
-from testingBot2 import testBot as tBot2
-from testingBot3 import testBot as tBot3
-from testingBot4 import testBot as tBot4
-from numericalSemigroupLite import*
 
-def legalMove(move, movesPlayed, remainingGaps, cap):
+def legalMove(move, movesPlayed, remainingGaps):
     if ((type(move) != int) or move < 1):
-        return False
-    if (move > cap):
         return False
     if (move in remainingGaps):
         return True
@@ -22,7 +17,7 @@ def legalMove(move, movesPlayed, remainingGaps, cap):
     return (move not in S)
 
 #PLAYING THE GAME:
-def SylverCoinageGame(Player1, Player2, cap = 200, numberOfGames = 100, startingPosition = []):
+def SylverCoinageGame(Player1, Player2, numberOfGames = 100, startingPosition = []):
     p1_wins = 0
     p2_wins = 0
     currentGame = 0
@@ -31,16 +26,20 @@ def SylverCoinageGame(Player1, Player2, cap = 200, numberOfGames = 100, starting
         print ("Game ", (currentGame + 1))
         p1_penalties = 0
         p2_penalties = 0
-        movesPlayed = [i for i in startingPosition]
+        if(0 in startingPosition):
+            remainingGaps = startingPosition[1:]
+            movesPlayed = []
+        else:
+            movesPlayed = [i for i in startingPosition]
         turn = (-1)**((currentGame + 1)+(len(movesPlayed)))
         while(1 not in movesPlayed):
             if(turn == -1):
-                move = Player1(movesPlayed, remainingGaps, cap)
+                move = Player1(movesPlayed, remainingGaps)
             else:
-                move = Player2(movesPlayed, remainingGaps, cap)
-            if legalMove(move, movesPlayed, remainingGaps, cap):
+                move = Player2(movesPlayed, remainingGaps)
+            if legalMove(move, movesPlayed, remainingGaps):
                 movesPlayed.append(move)
-                if (gcd_list(movesPlayed) != 1):
+                if ((gcd_list(movesPlayed) != 1) and (len(remainingGaps) == 0)):
                     turn = turn * (-1)
                 else:
                     if (len(remainingGaps) == 0):
@@ -94,10 +93,25 @@ def SylverCoinageGame(Player1, Player2, cap = 200, numberOfGames = 100, starting
     print("Final Score: ", [p1_wins, p2_wins])
     input("End.")
     return [p1_wins, p2_wins]
+
+def setCap(n):
+    return [i for i in range(0, n+1)]
+
+def checkForCap():
+    check = input('Would you like to cap the games? (y/n) ')
+    if(check == 'y'):
+        return True
+    elif(check == 'n'):
+        return False
+    else:
+        return checkForCap()
+    
     
 if __name__ == '__main__':
     numOfGames = int(input("How many games would you like to test the bot? "))
-    eachGameCap = int(input("What is the cap for move value (at least 10)?"))
-    if(eachGameCap < 10):
-        eachGameCap = 10
-    SylverCoinageGame(testBot().nextMove, myBot().nextMove, eachGameCap, numOfGames)
+    if(checkForCap()):
+        cap = int(input("Cap value: "))
+        SylverCoinageGame(testBot().nextMove, myBot().nextMove, numOfGames, setCap(cap))
+    else:
+        SylverCoinageGame(testBot().nextMove, myBot().nextMove, numOfGames)
+    
